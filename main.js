052eccd00bef64feaa529e994bbacc1697ff7151
Spektrum868 <55071936+Spektrum868@users.noreply.github.com>
2020-07-02 20:22:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Selectors
+
+// let header = document.querySelector('.header')
+let all = document.querySelector('.all')
+
+let hamburgerMenu = document.querySelector('.hamburger-menu')
+
+
+
+hamburgerMenu.addEventListener('click', function() {
+  // header.classList.toggle('menu-open');
+  all.classList.toggle('menu-open');
+})
